48194b8c176d67639f7d12dba9d3250664aa2391
VincentHenric <vincent.henric@hotmail.fr>
2018-08-05 21:29:31 +0100

    add requests

--- mockdbhelper.py
+++ mockdbhelper.py
@@ -1,3 +1,5 @@
+import datetime
+
 MOCK_USERS = [{
                 "email": "test@example.com",
                "salt": "gCioHZKNFMMsH6oJEJI+",
@@ -11,7 +13,16 @@ MOCK_TABLES = [{
                 "url": "mockurl"
               }]
 
+MOCK_REQUESTS = [{"_id": "1",
+                  "table_number": "1",
+                  "table_id": "1",
+                  "time": datetime.datetime.now()
+                }]
+
 class MockDBHelper:
+
+    def add_request(self, table_id, time):
+        MOCK_REQUESTS.append({"_id": table_id, "table_number": table_id, "table_id": table_id, "time": time})
 
     def add_table(self, number, owner):
         MOCK_TABLES.append({"_id": number, "number": number, "owner": owner})
@@ -20,11 +31,20 @@ class MockDBHelper:
     def add_user(self, email, salt, hashed):
         MOCK_USERS.append({"email":email, "salt": salt, "hashed": hashed})
 
+    def delete_request(self, request_id):
+        for i, request in enumerate(MOCK_REQUESTS):
+            if request["_id"] == request_id:
+                del MOCK_REQUESTS[i]
+                break
+
     def delete_table(self, table_id):
         for i, table in enumerate(MOCK_TABLES):
             if table.get("_id") == table_id:
                 del MOCK_TABLES[i]
                 break
+
+    def get_requests(self, owner_id):
+        return MOCK_REQUESTS
 
     def get_tables(self, owner_id):
         return MOCK_TABLES
--- waitercaller.py
+++ waitercaller.py
@@ -1,5 +1,6 @@
 from bitlyhelper import BitlyHelper
 import config
+import datetime
 from flask import Flask
 from flask.ext.login import current_user
 from flask.ext.login import LoginManager
@@ -57,7 +58,19 @@ def account_deletetable():
 @app.route("/dashboard")
 @login_required
 def dashboard():
-    return render_template("dashboard.html", routing_prefix = config.routing_prefix)
+    now = datetime.datetime.now()
+    requests = DB.get_requests(current_user.get_id())
+    for req in requests:
+        deltaseconds = (now - req['time']).seconds
+        req['wait_minutes'] = "{}.{}".format((deltaseconds/60), str(deltaseconds % 60).zfill(2))
+    return render_template("dashboard.html", requests = requests, routing_prefix = config.routing_prefix)
+
+@app.route("/dashboard/resolve")
+@login_required
+def dashboard_resolve():
+    request_id = request.args.get("request_id")
+    DB.delete_request(request_id)
+    return redirect(url_for('dashboard'))
 
 @app.route("/login", methods = ["POST"])
 def login():
@@ -76,6 +89,11 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("home"))
+
+@app.route("/newrequest/<tid>")
+def new_request(tid):
+    DB.add_request(tid, datetime.datetime.now())
+    return "Your request has been logged and a waiter will be with you shortly"
 
 @app.route("/register", methods = ['POST'])
 def register():
