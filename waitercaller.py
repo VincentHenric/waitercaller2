88cbc2ef8b8791a53156e7828acf0a146fad8239
VincentHenric <vincent.henric@hotmail.fr>
2018-08-10 20:38:23 +0100

    DBHelper code

--- waitercaller.py
+++ waitercaller.py
@@ -16,6 +16,8 @@ from forms import LoginForm
 from forms import RegistrationForm
 if config.test:
     from mockdbhelper import MockDBHelper as DBHelper
+else:
+    from dbhelper import DBHelper
 from passwordhelper import PasswordHelper
 from user import User
 
@@ -49,7 +51,7 @@ def account_createtable():
     form = CreateTableForm(request.form)
     if form.validate():
         tableid = DB.add_table(form.tablenumber.data, current_user.get_id())
-        new_url = BH.shorten_url(config.base_url + "newrequest/" + tableid)
+        new_url = BH.shorten_url(config.base_url + "newrequest/" + str(tableid))
         DB.update_table(tableid, new_url)
         return redirect(url_for('account'))
     return render_template('account.html', createtableform = form, tables = DB.get_tables(current_user.get_id()), routing_prefix = config.routing_prefix)
